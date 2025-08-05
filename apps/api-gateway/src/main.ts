b4944846c0b6edf026a6f18f598d1673d0b285f5
CoderNandan <nandanindia1947@gmail.com>
2025-08-06 01:34:42 +0530

    fix(api-gateway): ensure IPv6-safe rate limiting by using request-ip in keyGenerator

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import proxy from "express-http-proxy";
 import morgan from "morgan";
-import rateLimit from "express-rate-limit";
+import rateLimit, {ipKeyGenerator} from "express-rate-limit";
 import swaggerUi from "swagger-ui-express";
 import axios from "axios";
 import cookiParser from "cookie-parser";
@@ -30,8 +30,9 @@ const limiter = rateLimit({
   message: {error: "Too many requests, please try again later."},
   standardHeaders: true,
   legacyHeaders: true,
-  keyGenerator: (req: any) => req.ip,
+  keyGenerator: (req, res) => ipKeyGenerator(req.ip as string),
 });
+
 app.use(limiter);
 
 app.use("/assets", express.static(path.join(__dirname, "assets")));
